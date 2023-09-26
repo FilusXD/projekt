748870f42f1d3fabf205ed7b3450b0f3e706660f
Filip M <filipmasek1202@seznam.cz>
2023-09-26 14:52:16 +0200

    autoclick

--- res/js/main.js
+++ res/js/main.js
@@ -1,12 +1,35 @@
 const cookie = document.getElementById("cookie");
 const counter = document.getElementById("counter");
 const clickUpgrade = document.getElementById("clickUpgrade");
+const autoClickerUpgrade = document.getElementById("autoClickerUpgrade");
 
 let numberOfCookies = 0;
 let costOfClickUpgrade = 20;
 let clickUpgradeIncrease = 1;
+let autoClickIncrease = 0;
+let autoclicker;
+let autoclickerCost = 100;
 
-
+autoClickerUpgrade.onclick = () => {
+    if (numberOfCookies >= autoclickerCost){
+        //odecist cookies
+        numberOfCookies -= autoclickerCost;
+        autoclickerCost *= 2;
+        autoClickerUpgrade.innerText = "Buy autoclick upgrade: " + autoclickerCost;
+        //aktualizovat odstavec se susenkama
+        counter.innerHTML = "FazCoins: " + numberOfCookies;
+        autoClickIncrease++;
+        //clear
+        clearInterval(autoclicker);
+        //spustit interval
+       autoclicker = setInterval(() => {
+            //zvednout o nejake cislo
+        numberOfCookies += autoClickIncrease
+            //aktualizovat odstavec
+         counter.innerHTML = "FazCoins: " + numberOfCookies;
+        }, 1000);
+    } 
+}
 cookie.onclick = () => {
     console.log("click");
     numberOfCookies += clickUpgradeIncrease;
@@ -21,7 +44,7 @@ const clickUpgradeFunction = () => {
         numberOfCookies -= costOfClickUpgrade;
 
         //aktualizace odstavce
-        counter.innerText = "Fazcoins: " + numberOfCookies;
+        counter.innerText = "FazCoins: " + numberOfCookies;
         //zvedneme pocet susenek na kliknutí
         clickUpgradeIncrease++;
     }
@@ -32,11 +55,12 @@ clickUpgrade.onclick = clickUpgradeFunction;
 function cheats() {
     console.log("Cheats on");
     numberOfCookies += 1000000;
-   counter.innerText = "Fazcoins: " + numberOfCookies;
+   counter.innerText = "FazCoins: " + numberOfCookies;
 }
 
 const cheatsTwo = () => {
     numberOfCookies *= 2;
-    counter.innerText = "Fazcoins: " + numberOfCookies;
+    counter.innerText = "FazCoins: " + numberOfCookies;
 }
-
+cheats();
+cheatsTwo();
