455044e170b7d750e269f9febb61b73e65468ccf
Filip M <filipmasek1202@seznam.cz>
2023-10-05 20:22:02 +0200

    silly div doesn't work

--- res/js/main.js
+++ res/js/main.js
@@ -2,14 +2,35 @@ const cookie = document.getElementById("cookie");
 const counter = document.getElementById("counter");
 const clickUpgrade = document.getElementById("clickUpgrade");
 const autoClickerUpgrade = document.getElementById("autoClickerUpgrade");
+const autoClickerUpgrade2 = document.getElementById("autoClickerUpgrade2");
 
 let numberOfCookies = 0;
 let costOfClickUpgrade = 20;
 let clickUpgradeIncrease = 1;
 let autoClickIncrease = 0;
+let autoClickIncrease2 = 0;
 let autoclicker;
 let autoclickerCost = 100;
+let autoclicker2Cost = 1000;
 
+
+autoClickerUpgrade2.onclick = () => {
+    if (numberOfCookies >= autoclicker2Cost){
+        numberOfCookies -= autoclicker2Cost;
+        autoclicker2Cost *= 2;
+        autoClickerUpgrade.innerText = "Buy Markiplier upgrade: " + autoclicker2Cost;
+        counter.innerHTML = "FazCoins: " + numberOfCookies;
+        autoClickIncrease2++;
+        autoClickIncrease2 += 9;
+        clearInterval(autoclicker)
+       autoclicker = setInterval(() => {
+        numberOfCookies += autoClickIncrease2;
+        numberOfCookies += autoClickIncrease;
+         counter.innerHTML = "FazCoins: " + numberOfCookies;
+        },  1000);
+        
+    } 
+}
 
 
 autoClickerUpgrade.onclick = () => {
@@ -17,22 +38,25 @@ autoClickerUpgrade.onclick = () => {
         //odecist cookies
         numberOfCookies -= autoclickerCost;
         autoclickerCost *= 2;
-        autoClickerUpgrade.innerText = "Buy autoclick upgrade: " + autoclickerCost;
+        autoClickerUpgrade.innerText = "Buy William upgrade: " + autoclickerCost;
         //aktualizovat odstavec se susenkama
         counter.innerHTML = "FazCoins: " + numberOfCookies;
         autoClickIncrease++;
+        autoClickIncrease += 0;
         //clear
-        clearInterval(autoclicker);
+        clearInterval(autoclicker)
         //spustit interval
        autoclicker = setInterval(() => {
             //zvednout o nejake cislo
-        numberOfCookies += autoClickIncrease
+        numberOfCookies += autoClickIncrease;
+        numberOfCookies += autoClickIncrease2;
             //aktualizovat odstavec
          counter.innerHTML = "FazCoins: " + numberOfCookies;
-         updateInfo()
         }, 1000);
     } 
 }
+
+
 cookie.onclick = () => {
     console.log("click");
     numberOfCookies += clickUpgradeIncrease;
@@ -45,7 +69,7 @@ const clickUpgradeFunction = () => {
         //odečteme cenu upgradu
         // pocet susenek a odectu 20
         numberOfCookies -= costOfClickUpgrade;
-
+        costOfClickUpgrade *= 2;
         //aktualizace odstavce
         counter.innerText = "FazCoins: " + numberOfCookies;
         //zvedneme pocet susenek na kliknutí
@@ -66,4 +90,3 @@ const cheatsTwo = () => {
     counter.innerText = "FazCoins: " + numberOfCookies;
 }
 cheats();
-cheatsTwo();
