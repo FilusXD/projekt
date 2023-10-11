e345019dcf27a7e243b33630fad86cdaebdafd23
Filip M <filipmasek1202@seznam.cz>
2023-10-11 20:32:42 +0200

    div se rozhodl spolupracovat peepoHappy

--- res/js/main.js
+++ res/js/main.js
@@ -1,17 +1,45 @@
 const cookie = document.getElementById("cookie");
 const counter = document.getElementById("counter");
 const clickUpgrade = document.getElementById("clickUpgrade");
+const clickUpgrade2 = document.getElementById("clickUpgrade2");
+const clickUpgrade3 = document.getElementById("clickUpgrade3");
 const autoClickerUpgrade = document.getElementById("autoClickerUpgrade");
 const autoClickerUpgrade2 = document.getElementById("autoClickerUpgrade2");
+const autoClickerUpgrade3 = document.getElementById("autoClickerUpgrade3");
 
 let numberOfCookies = 0;
 let costOfClickUpgrade = 20;
+let costOfClickUpgrade2 = 750;
+let costOfClickUpgrade3 = 5000;
 let clickUpgradeIncrease = 1;
+let clickUpgradeIncrease2 = 1;
 let autoClickIncrease = 0;
 let autoClickIncrease2 = 0;
+let autoClickIncrease3 = 0;
 let autoclicker;
 let autoclickerCost = 100;
 let autoclicker2Cost = 1000;
+let autoclicker3Cost = 15000;
+
+
+autoClickerUpgrade3.onclick = () => {
+    if (numberOfCookies >= autoclicker3Cost){
+        numberOfCookies -= autoclicker3Cost;
+        autoclicker2Cost *= 2;
+        autoClickerUpgrade.innerText = "Buy Freddy: " + autoclicker3Cost;
+        counter.innerHTML = "FazCoins: " + numberOfCookies;
+        autoClickIncrease3++;
+        autoClickIncrease3 += 199;
+        clearInterval(autoclicker)
+       autoclicker = setInterval(() => {
+        numberOfCookies += autoClickIncrease3;
+        numberOfCookies += autoClickIncrease2;
+        numberOfCookies += autoClickIncrease;
+         counter.innerHTML = "FazCoins: " + numberOfCookies;
+        },  1000);
+        
+    } 
+}
 
 
 autoClickerUpgrade2.onclick = () => {
@@ -24,6 +52,7 @@ autoClickerUpgrade2.onclick = () => {
         autoClickIncrease2 += 9;
         clearInterval(autoclicker)
        autoclicker = setInterval(() => {
+        numberOfCookies += autoClickIncrease3;
         numberOfCookies += autoClickIncrease2;
         numberOfCookies += autoClickIncrease;
          counter.innerHTML = "FazCoins: " + numberOfCookies;
@@ -35,22 +64,17 @@ autoClickerUpgrade2.onclick = () => {
 
 autoClickerUpgrade.onclick = () => {
     if (numberOfCookies >= autoclickerCost){
-        //odecist cookies
         numberOfCookies -= autoclickerCost;
         autoclickerCost *= 2;
         autoClickerUpgrade.innerText = "Buy William upgrade: " + autoclickerCost;
-        //aktualizovat odstavec se susenkama
         counter.innerHTML = "FazCoins: " + numberOfCookies;
         autoClickIncrease++;
         autoClickIncrease += 0;
-        //clear
         clearInterval(autoclicker)
-        //spustit interval
        autoclicker = setInterval(() => {
-            //zvednout o nejake cislo
+        numberOfCookies += autoClickIncrease3;
         numberOfCookies += autoClickIncrease;
         numberOfCookies += autoClickIncrease2;
-            //aktualizovat odstavec
          counter.innerHTML = "FazCoins: " + numberOfCookies;
         }, 1000);
     } 
@@ -64,7 +88,10 @@ cookie.onclick = () => {
     counter.innerHTML = "FazCoins: " + numberOfCookies;
 };
 
-const clickUpgradeFunction = () => {
+
+
+
+clickUpgrade.onclick = () => {
     if (numberOfCookies >= costOfClickUpgrade){
         //odečteme cenu upgradu
         // pocet susenek a odectu 20
@@ -77,13 +104,28 @@ const clickUpgradeFunction = () => {
     }
 };
 
-clickUpgrade.onclick = clickUpgradeFunction;
+clickUpgrade2.onclick = () => {
+    if (numberOfCookies >= costOfClickUpgrade2){
+        //odečteme cenu upgradu
+        // pocet susenek a odectu 20
+        numberOfCookies -= costOfClickUpgrade2;
+        costOfClickUpgrade2 *= 2;
+        //aktualizace odstavce
+        counter.innerText = "FazCoins: " + numberOfCookies;
+        //zvedneme pocet susenek na kliknutí
+        clickUpgradeIncrease++;
+        clickUpgradeIncrease += 23;
+    }
+};
+
+
+
 
 function cheats() {
     console.log("Cheats on");
     numberOfCookies += 1000000;
    counter.innerText = "FazCoins: " + numberOfCookies;
-}
+};
 
 const cheatsTwo = () => {
     numberOfCookies *= 2;
